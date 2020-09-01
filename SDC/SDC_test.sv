4efff8fba21683d09a26946896cf94d439bc607d
Ariel Cohen <65937178+Ariel-fsae@users.noreply.github.com>
2020-09-01 20:23:57 +0300

    remade the test, needs fixing still

--- SDC/SDC_test.sv
+++ SDC/SDC_test.sv
@@ -13,61 +13,27 @@ logic SDCready;
 newSDC dut (.To_SDC_relais(SDCrelais), .SDC_is_Ready(SDCready),
  .AS_close_SDC(SDC_close), .AS_driving_mode(driving_mode),
  .TS_Activation_Button_cockpit(manual_ignition), .TS_Activation_Button_external(Auto_ignition),
- .Power_on_Reset(PowerOnreset), .Watchdog(WatchdogOutput), .Shutdown_circuit(Shutdown));
+ .Power_on_Reset(PowerOnreset), .Watchdog(WatchdogOutput), .Shutdown_circuit(SDCrelais));
 
  initial begin
-     //sets all to zero as the car and all its systems are turnedoff
-     PowerOnreset = 1'b0;
-     driving_mode = 1'b0;
-     SDC_close = 1'b0;
-     WatchdogOutput = 1'b0;
-     manual_ignition = 1'b0;
-     Auto_ignition = 1'b0;
-     Shutdown = 1'b0;
+     //resetting the system
+
+     PowerOnreset=1'b1;
 
      #10
 
-     PowerOnreset = 1'b1;
+     WatchdogOutput= 1'b1;
+     PowerOnreset = 1'b0;
+     #10
      SDC_close = 1'b1;
-     WatchdogOutput = 1'b1;
-     Shutdown = 1'b1;
-    
-    #10
+     driving_mode = 1'b1;
+     Auto_ignition = 1'b1;
+     #1
+     Auto_ignition = 1'b0;
+     #9
+     WatchdogOutput = 1'b0;
 
-    manual_ignition = 1'b1;
-    #1 // creating a pulse
-    manual_ignition = 1'b0;
-
-    #10
-    // reseting the system
-
-    PowerOnreset = 1'b0;
-    #1
-    PowerOnreset = 1'b1;
-    // turning on the system in the wrong mode
-    #5
-
-    Auto_ignition = 1'b1;
-    #1 // creating a pulse
-    Auto_ignition = 1'b0;
-
-    #10 //reseting the system
-    PowerOnreset = 1'b0;
-    #1
-    PowerOnreset = 1'b1;
-    #5
-    // turning on Autonamus capabilities while it is on the right mode
-    driving_mode = 1'b1;
-    Auto_ignition = 1'b1;
-    #1 // creating a pulse
-    Auto_ignition = 1'b0;
-    
-    
-    
-
-
-
-
+     
 
  end
 endmodule
